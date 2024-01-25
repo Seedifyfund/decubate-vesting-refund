cdcf7555016e6d80e8583a8992b7610566ff9e4e
Théo RICHARD <mail@rcode.tech>
2024-01-25 16:08:20 +0100

    chore(refactor): openzeppelin reamappings

--- src/IGOVesting.sol
+++ src/IGOVesting.sol
@@ -3,8 +3,8 @@
 
 pragma solidity ^0.8.17;
 
-import {AccessControlUpgradeable} from "@openzeppelin-contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import {SafeERC20Upgradeable, IERC20Upgradeable} from "@openzeppelin-contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+import {AccessControlUpgradeable} from "openzeppelin-contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import {SafeERC20Upgradeable, IERC20Upgradeable} from "openzeppelin-contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 
 import {IIGOVesting} from "./interfaces/IIGOVesting.sol";
 
